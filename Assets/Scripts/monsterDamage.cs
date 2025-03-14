0bfc6dea02cc86322ca06ab9fe2e34bd69e7f0ee
d0r5z <aleksandra.fran09@gmail.com>
2025-03-14 13:06:24 +0100

    cos

--- Assets/Scripts/monsterDamage.cs
+++ Assets/Scripts/monsterDamage.cs
@@ -6,11 +6,14 @@ public class monsterDamage : MonoBehaviour
     [SerializeField] public int damage;
     public playerHealth playerHealth;
 
+    
+    
 private void OnCollisionEnter2D(Collision2D collision)
 {
     if(collision.gameObject.tag == "Player")
     {
         playerHealth.TakeDamage(damage);
+        
     }
 }
  
--- Assets/Scripts/playerHealth.cs
+++ Assets/Scripts/playerHealth.cs
@@ -5,11 +5,14 @@ public class playerHealth : MonoBehaviour
 
     public int MaxHealth = 10;
     public int health;
+    private Rigidbody2D rb;
+    [SerializeField] public float PushForce;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         health = MaxHealth;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,6 +25,7 @@ public class playerHealth : MonoBehaviour
 public void TakeDamage(int damage) 
     {
         health -= damage;
+        rb.linearVelocity = new Vector2(PushForce, rb.linearVelocity.y);
         if(health <= 0)
         {
             Destroy(gameObject);
