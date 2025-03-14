a2f88806394f3a9fac462c166eaf5988d5db3801
Maciek1607 <maciek.dabrowa.2008@gmail.com>
2025-03-14 23:11:01 +0100

    ODPYCHA SIE PO UDERZENIU!!!

--- Assets/Scripts/Player_Codes/CrowMovement.cs
+++ Assets/Scripts/Player_Codes/CrowMovement.cs
@@ -2,16 +2,42 @@ using UnityEngine;
 
 public class CrowMovement : MonoBehaviour
 {
-    private float Direction;
+    [SerializeField] public float PushSpeed;
+    public float PushTime;
+    public float PushCounter;
+    public bool PushFromRight;
+    public bool PushFromLeft;
+    public float Direction;
     private Rigidbody2D rb;
     [SerializeField] private float Speed;
 
     void Start()
     {rb = GetComponent<Rigidbody2D>();}
-    void Update()
+    void FixedUpdate()
     {
         Direction = Input.GetAxis("Horizontal");
         Direction = Direction * Speed;
-        rb.linearVelocity = new Vector2(Direction, rb.linearVelocity.y);
+        if(PushCounter <= 0)
+        {rb.linearVelocity = new Vector2(Direction, rb.linearVelocity.y);
+        PushFromLeft = false;
+        PushFromRight = false;}
+
+        else{
+            if(PushFromRight == true)
+            {
+                rb.linearVelocity = new Vector2(-PushSpeed, PushSpeed / 10);
+            }
+            if(PushFromLeft == true)
+            {
+                rb.linearVelocity = new Vector2(PushSpeed, PushSpeed / 10);
+            }
+            PushCounter -= Time.deltaTime;
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        Direction = Direction * PushSpeed;
     }
 }
--- Assets/Scripts/monsterDamage.cs
+++ Assets/Scripts/monsterDamage.cs
@@ -2,19 +2,31 @@ using UnityEngine;
 
 public class monsterDamage : MonoBehaviour
 {
-
-    [SerializeField] public float damage;
+    private Rigidbody2D rb;
+    public CrowMovement cm;
+    [SerializeField] public int damage;
     public playerHealth playerHealth;
 
-    
+void Start()
+{
+    rb = GetComponent<Rigidbody2D>();
+}
     
 private void OnCollisionEnter2D(Collision2D collision)
 {
     if(collision.gameObject.tag == "Player")
     {
+        cm.PushCounter = cm.PushTime;
+        if(collision.transform.position.x < transform.position.x)
+        {
+            cm.PushFromRight = true;
+        }
+        if(collision.transform.position.x > transform.position.x)
+            cm.PushFromLeft = true;
         playerHealth.TakeDamage(damage);
-        
-    }
+
+
+        }
 }
  
 }
